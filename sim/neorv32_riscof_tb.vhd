c96ab5e6f4907805f18f1f8ec9b2ce4c4aecdadc
Iztok Jeras <iztok.jeras@gmail.com>
2025-12-03 21:41:42 +0100

    working and not much memory

--- sim/neorv32_riscof_tb.vhd
+++ sim/neorv32_riscof_tb.vhd
@@ -30,54 +30,40 @@ architecture neorv32_riscof_tb_rtl of neorv32_riscof_tb is
   constant mem_size_c : natural := 4*1024*1024; -- bytes
   constant mem_base_c : std_ulogic_vector(31 downto 0) := x"80000000";
 
-  -- memory file path&name
-  constant MEM_FILE : string := TEST_PATH & "main.bin";
+  -- memory file path&name (max 4MB)
+  constant MEM_FILE : string := TEST_PATH & "main.hex";
 
   -- memory type --
   type mem8_bv_t is array (natural range <>) of bit_vector(7 downto 0); -- bit_vector type for optimized system storage
 
-  -- initialize mem8_bv_t array from plain binary file --
-  impure function mem8_bv_init_f(file_name : string; num_words : natural) return mem8_bv_t is
-    type char_file is file of character;
-    file     mem_f   : char_file;
-    variable mem_v   : mem8_bv_t(0 to num_words-1);
-    variable index_v : natural;
-    variable data_v  : character;
+  -- initialize mem8_bv_t array from plain ASCII HEX file  --
+  impure function mem8_bv_init_f(file_name : string; num_words : natural; byte_sel : natural) return mem8_bv_t is
+    file     text_file   : text open read_mode is file_name;
+    variable text_line_v : line;
+    variable mem8_bv_v   : mem8_bv_t(0 to num_words-1);
+    variable index_v     : natural;
+    variable word_v      : bit_vector(31 downto 0);
   begin
+    mem8_bv_v := (others => (others => '0'));
+    index_v   := 0;
     if (file_name /= "") then
-      file_open(mem_f, MEM_FILE, READ_MODE);
-      index_v := 0;
-      while (endfile(mem_f) = false) and (index_v < num_words) loop
-        read(mem_f, data_v);
-        mem_v(index_v) := to_bitvector(std_logic_vector(to_unsigned(character'pos(data_v),8)));
+      while (endfile(text_file) = false) and (index_v < num_words) loop
+        readline(text_file, text_line_v);
+        hread(text_line_v, word_v);
+        case byte_sel is
+          when 0      => mem8_bv_v(index_v) := word_v(07 downto 00);
+          when 1      => mem8_bv_v(index_v) := word_v(15 downto 08);
+          when 2      => mem8_bv_v(index_v) := word_v(23 downto 16);
+          when others => mem8_bv_v(index_v) := word_v(31 downto 24);
+        end case;
         index_v := index_v + 1;
       end loop;
     end if;
-    file_close(mem_f);
-    return mem_v;
+    return mem8_bv_v;
   end function mem8_bv_init_f;
 
-  -- dump mem8_bv_t array to plain binary file --
-  procedure mem8_bv_dump_f(file_name : string; mem_v : mem8_bv_t) is
-    type char_file is file of character;
-    file     mem_f   : char_file;
-    variable data_v  : character;
-  begin
-    if (file_name /= "") then
-      file_open(mem_f, file_name, WRITE_MODE);
-      for index_v in 0 to mem_v'length-1 loop
-        data_v := character'val(to_integer(unsigned(to_stdlogicvector(mem_v(index_v)))));
-        write(mem_f, data_v);
-      end loop;
-    end if;
-    file_close(mem_f);
-  end procedure mem8_bv_dump_f;
-
-  -- memory array --
-  signal mem8_v : mem8_bv_t(0 to mem_size_c-1) := mem8_bv_init_f(MEM_FILE, mem_size_c);
-
   -- memory word address --
-  signal mem_addr : integer range 0 to mem_size_c-1;
+  signal mem_addr : integer range 0 to (mem_size_c/4)-1;
 
   -- generators --
   signal clk_gen, rst_gen : std_ulogic := '0';
@@ -155,7 +141,6 @@ begin
     IO_TRACER_EN        => true,
     IO_TRACER_BUFFER    => 1,
     IO_TRACER_SIMLOG_EN => true
---    IO_TRACER_SIMLOG_FILE => TEST_PATH & "DUT-neorv32.log"
   )
   port map (
     -- Global control --
@@ -181,9 +166,14 @@ begin
   xbus.rdata <= mem_rdata;
   xbus.ack   <= mem_ack or env_ack;
 
+
   -- Main Memory [rwx] - Constructed from four parallel byte-wide memories ------------------
   -- -------------------------------------------------------------------------------------------
   main_mem: process(clk_gen)
+    variable mem8_bv_b0_v : mem8_bv_t(0 to (mem_size_c/4)-1) := mem8_bv_init_f(MEM_FILE, mem_size_c/4, 0);
+    variable mem8_bv_b1_v : mem8_bv_t(0 to (mem_size_c/4)-1) := mem8_bv_init_f(MEM_FILE, mem_size_c/4, 1);
+    variable mem8_bv_b2_v : mem8_bv_t(0 to (mem_size_c/4)-1) := mem8_bv_init_f(MEM_FILE, mem_size_c/4, 2);
+    variable mem8_bv_b3_v : mem8_bv_t(0 to (mem_size_c/4)-1) := mem8_bv_init_f(MEM_FILE, mem_size_c/4, 3);
   begin
     if rising_edge(clk_gen) then
       -- defaults --
@@ -193,22 +183,22 @@ begin
       if (xbus.cyc = '1') and (xbus.stb = '1') and (xbus.addr(31 downto 28) = mem_base_c(31 downto 28)) then
         mem_ack <= '1';
         if (xbus.we = '1') then
-          if (xbus.sel(0) = '1') then mem8_v(mem_addr+0) <= to_bitvector(xbus.wdata(07 downto 00)); end if;
-          if (xbus.sel(1) = '1') then mem8_v(mem_addr+1) <= to_bitvector(xbus.wdata(15 downto 08)); end if;
-          if (xbus.sel(2) = '1') then mem8_v(mem_addr+2) <= to_bitvector(xbus.wdata(23 downto 16)); end if;
-          if (xbus.sel(3) = '1') then mem8_v(mem_addr+3) <= to_bitvector(xbus.wdata(31 downto 24)); end if;
+          if (xbus.sel(0) = '1') then mem8_bv_b0_v(mem_addr) := to_bitvector(xbus.wdata(07 downto 00)); end if;
+          if (xbus.sel(1) = '1') then mem8_bv_b1_v(mem_addr) := to_bitvector(xbus.wdata(15 downto 08)); end if;
+          if (xbus.sel(2) = '1') then mem8_bv_b2_v(mem_addr) := to_bitvector(xbus.wdata(23 downto 16)); end if;
+          if (xbus.sel(3) = '1') then mem8_bv_b3_v(mem_addr) := to_bitvector(xbus.wdata(31 downto 24)); end if;
         else
-          mem_rdata(07 downto 00) <= to_stdulogicvector(mem8_v(mem_addr+0));
-          mem_rdata(15 downto 08) <= to_stdulogicvector(mem8_v(mem_addr+1));
-          mem_rdata(23 downto 16) <= to_stdulogicvector(mem8_v(mem_addr+2));
-          mem_rdata(31 downto 24) <= to_stdulogicvector(mem8_v(mem_addr+3));
+          mem_rdata(07 downto 00) <= to_stdulogicvector(mem8_bv_b0_v(mem_addr));
+          mem_rdata(15 downto 08) <= to_stdulogicvector(mem8_bv_b1_v(mem_addr));
+          mem_rdata(23 downto 16) <= to_stdulogicvector(mem8_bv_b2_v(mem_addr));
+          mem_rdata(31 downto 24) <= to_stdulogicvector(mem8_bv_b3_v(mem_addr));
         end if;
       end if;
     end if;
   end process main_mem;
 
   -- read/write address --
-  mem_addr <= to_integer(unsigned(xbus.addr(index_size_f(mem_size_c/4)+1 downto 2))) * 4;
+  mem_addr <= to_integer(unsigned(xbus.addr(index_size_f(mem_size_c/4)+1 downto 2)));
 
 
   -- Environment Control --------------------------------------------------------------------
