55808ce315426d721f742c36e3dec21323c695f3
Iztok Jeras <iztok.jeras@gmail.com>
2025-12-11 08:22:37 +0100

    file renames

--- sim/ghdl_setup.sh
+++ sim/ghdl_setup.sh
@@ -7,7 +7,7 @@ cd $(dirname "$0")
 NEORV32_RTL=${NEORV32_RTL:-../neorv32/rtl}
 SRC_FOLDER=${SRC_FOLDER:-.}
 
-TB="neorv32_io_pkg.vhd neorv32_riscof_tb.vhd"
+TB="neorv32_io_pkg.vhd neorv32_riscof_tb_vhdl2008.vhd"
 
 ghdl -i --work=neorv32 --std=08 "$NEORV32_RTL"/core/*.vhd $TB
 ghdl -m --std=08 --work=neorv32 neorv32_riscof_tb
