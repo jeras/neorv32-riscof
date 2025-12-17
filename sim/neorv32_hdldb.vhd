17225bcd0202174a86c08afa7b0a9c172876daf3
Iztok Jeras <iztok.jeras@gmail.com>
2025-12-17 15:47:38 +0100

    some progress on trace, unfinished

--- sim/neorv32_hdldb.vhd
+++ sim/neorv32_hdldb.vhd
@@ -36,7 +36,7 @@ end neorv32_hdldb;
 
 architecture neorv32_hdldb_behav of neorv32_hdldb is
 
-  -- trace port
+  -- instruction trace port
   signal trace : trace_port_t; -- CPU trace port
 
 begin
@@ -61,16 +61,18 @@ begin
     end loop;
 
     -- sample state
-    gpr_v := << signal .neorv32_riscof_tb.neorv32_top_inst.core_complex_gen(0).neorv32_cpu_inst.neorv32_cpu_regfile_inst.register_file_fpga.reg_file_inst.sdpram : gpr_v'subtype >>;
-    pc_v  := << signal .neorv32_riscof_tb.neorv32_top_inst.core_complex_gen(0).neorv32_cpu_inst.neorv32_cpu_frontend_inst.fetch.pc : pc_v'subtype >>;
+--    gpr_v := << signal .neorv32_riscof_tb.neorv32_top_inst.core_complex_gen(0).neorv32_cpu_inst.neorv32_cpu_regfile_inst.register_file_fpga.reg_file_inst.sdpram : gpr_v'subtype >>;
+--    pc_v  := << signal .neorv32_riscof_tb.neorv32_top_inst.core_complex_gen(0).neorv32_cpu_inst.neorv32_cpu_frontend_inst.fetch.pc : pc_v'subtype >>;
 --  csr_v := (
 --    16#000# => << signal .neorv32_riscof_tb.neorv32_top_inst.neorv32_cpu_inst.,
 --    others  => 32X"00000000"
 --  );
-    mem_v := << signal .neorv32_riscof_tb.main.array8_v : mem_v'subtype >>;
+--    mem_v := << variable .neorv32_riscof_tb.main.array8_v : mem_v'subtype >>;
     
     -- dump state
-    dump_bin(dumpfile, gpr_v);
+--    dump_bin(dumpfile, gpr_v);
+--    dump_bin(dumpfile, pc_v);
+--    dump_bin(dumpfile, mem_v);
     wait;
   end process state_dump;
 
@@ -80,5 +82,73 @@ begin
 
   trace <= << signal .neorv32_riscof_tb.neorv32_top_inst.trace_cpu0_o : trace_port_t >>;
 
+  trace_log: process
+    variable filename  : string := "neorv32.hdldb-trace";
+    file     tracefile : char_file;
+    variable timestamp : bit_vector(64-1 downto 0);
+  begin
+    -- open trace log file
+    file_open(tracefile, filename, WRITE_MODE);
+    loop
+      -- on every clock cycle check the trace valid signal
+      wait until rising_edge(clk);
+      -- log if instruction trace is valid
+      if (rst = '0') and (trace.valid = '1') then
+        -- header
+        -- [31:28] trace packet size in XLEN units
+        -- [27   ] memory access write
+        -- [   26] memory access read
+        -- [25:24] memory access size (0 is byte, ...)
+        --  
+        -- timestamp
+        write_bin(tracefile, to_bitvector(std_logic_vector(to_signed(now/(1 fs), 64))));
+        -- IFU
+        write_bin(tracefile, trace.pc_rdata);  -- instruction address (PC)
+        write_bin(tracefile, trace.insn    );  -- instruction code
+        -- GPR
+        write_bin(tracefile, trace.rd_rdata);  -- destination register data
+        -- LSU
+        write_bin(tracefile, trace.mem_addr);  -- memory address
+      end if;
+    end loop;
+    -- close trace log file
+    file_close(tracefile);
+    wait;
+  end process trace_log;
+
 
 end neorv32_hdldb_behav;
+
+--  type trace_port_t is record
+--    valid     : std_ulogic; -- all other signals are valid when set
+--    -- instruction metadata --
+--    order     : std_ulogic_vector(31 downto 0); -- instruction index
+--    insn      : std_ulogic_vector(31 downto 0); -- instruction word
+--    trap      : std_ulogic; -- set if the current instruction causes a sync exception
+--    halt      : std_ulogic; -- set if last instruction before halting
+--    intr      : std_ulogic; -- set if executing the first instruction of a trap handler
+--    mode      : std_ulogic_vector(1 downto 0); -- 00 = user mode, 11 = machine mode
+--    ixl       : std_ulogic_vector(1 downto 0); -- XLEN; 01 = 32-bit
+--    debug     : std_ulogic; -- set if instruction is executed in debug-mode
+--    compr     : std_ulogic; -- set if instruction is a decompressed instruction
+--    -- integer register --
+--    rs1_addr  : std_ulogic_vector(4 downto 0);  -- rs1 address
+--    rs2_addr  : std_ulogic_vector(4 downto 0);  -- rs2 address
+--    rs1_rdata : std_ulogic_vector(31 downto 0); -- rs1 read data
+--    rs2_rdata : std_ulogic_vector(31 downto 0); -- rs2 read data
+--    rd_addr   : std_ulogic_vector(4 downto 0);  -- rd address
+--    rd_rdata  : std_ulogic_vector(31 downto 0); -- rd write data
+--    -- program counter --
+--    pc_rdata  : std_ulogic_vector(31 downto 0); -- current instruction address
+--    pc_wdata  : std_ulogic_vector(31 downto 0); -- next instruction address
+--    -- control and status register --
+--    csr_addr  : std_ulogic_vector(11 downto 0); -- csr address
+--    csr_rdata : std_ulogic_vector(31 downto 0); -- csr read data
+--    csr_wdata : std_ulogic_vector(31 downto 0); -- csr write data
+--    -- memory access --
+--    mem_addr  : std_ulogic_vector(31 downto 0); -- address
+--    mem_rmask : std_ulogic_vector(3 downto 0);  -- read-enable
+--    mem_wmask : std_ulogic_vector(3 downto 0);  -- write-enable
+--    mem_rdata : std_ulogic_vector(31 downto 0); -- read data
+--    mem_wdata : std_ulogic_vector(31 downto 0); -- write data
+--  end record;
--- sim/neorv32_hdldb_io_pkg.vhd
+++ sim/neorv32_hdldb_io_pkg.vhd
@@ -16,58 +16,73 @@ use ieee.numeric_std.all;
 
 package neorv32_hdldb_io_pkg is
 
+  -- file types
+  type char_file is file of character;
+
+  procedure write_bin(file f : char_file; vector_i : bit_vector);
+  procedure write_bin(file f : char_file; vector_i : std_logic_vector);
+
   -- generic types
   type array_bit_vector       is array (natural range <>) of bit_vector;
   type array_std_logic_vector is array (natural range <>) of std_logic_vector;
   -- generic functions/procedures
-  procedure dump_bin(file_name : string; vector_i :      bit_vector      ; mode : file_open_kind := WRITE_MODE);
-  procedure dump_bin(file_name : string; array_i : array_bit_vector      ; mode : file_open_kind := WRITE_MODE);
-  procedure dump_bin(file_name : string; vector_i :      std_logic_vector; mode : file_open_kind := WRITE_MODE);
-  procedure dump_bin(file_name : string; array_i : array_std_logic_vector; mode : file_open_kind := WRITE_MODE);
+  procedure dump_bin(file_name : in string; vector_i : in       bit_vector      ; mode : in file_open_kind := WRITE_MODE);
+  procedure dump_bin(file_name : in string; array_i  : in array_bit_vector      ; mode : in file_open_kind := WRITE_MODE);
+  procedure dump_bin(file_name : in string; vector_i : in       std_logic_vector; mode : in file_open_kind := WRITE_MODE);
+  procedure dump_bin(file_name : in string; array_i  : in array_std_logic_vector; mode : in file_open_kind := WRITE_MODE);
 
 end package neorv32_hdldb_io_pkg;
 
 package body neorv32_hdldb_io_pkg is
 
+  procedure write_bin(file f : char_file; vector_i : bit_vector) is
+    variable data_v : character;
+  begin
+    for byte_v in 0 to vector_i'length/8-1 loop
+      data_v := character'val(to_integer(unsigned(to_stdlogicvector(vector_i((byte_v+1)*8-1 downto (byte_v+0)*8)))));
+      write(f, data_v);
+    end loop;
+  end procedure write_bin;
+
+  procedure write_bin(file f : char_file; vector_i : std_logic_vector) is
+    variable data_v : character;
+  begin
+    for byte_v in 0 to vector_i'length/8-1 loop
+      data_v := character'val(to_integer(unsigned(vector_i((byte_v+1)*8-1 downto (byte_v+0)*8))));
+      write(f, data_v);
+    end loop;
+  end procedure write_bin;
+
+
   -- dump array_bit_vector to plain binary file --
   procedure dump_bin(
-    file_name : string;
-    vector_i  : bit_vector;
-    mode      : file_open_kind := WRITE_MODE
+    file_name : in  string;
+    vector_i  : in  bit_vector;
+    mode      : in  file_open_kind := WRITE_MODE
   ) is
-    type char_file is file of character;
     file     dump_f : char_file;
     variable data_v : character;
   begin
     if (file_name /= "") then
       file_open(dump_f, file_name, mode);
-      for byte_v in 0 to vector_i'length/8-1 loop
-        data_v := character'val(to_integer(unsigned(to_stdlogicvector(vector_i((byte_v+1)*8-1 downto (byte_v+0)*8)))));
-        write(dump_f, data_v);
-      end loop;
+      write_bin(dump_f, vector_i);
     end if;
     file_close(dump_f);
   end procedure dump_bin;
 
   -- dump array_bit_vector array to plain binary file --
   procedure dump_bin(
-    file_name : string;
-    array_i   : array_bit_vector;
-    mode      : file_open_kind := WRITE_MODE
+    file_name : in  string;
+    array_i   : in  array_bit_vector;
+    mode      : in  file_open_kind := WRITE_MODE
   ) is
-    type char_file is file of character;
     file     dump_f   : char_file;
-    variable vector_i : array_i'element'subtype;
     variable data_v   : character;
   begin
     if (file_name /= "") then
       file_open(dump_f, file_name, mode);
       for index_v in array_i'range loop
-        vector_i := array_i(index_v);
-        for byte_v in 0 to vector_i'length/8-1 loop
-          data_v := character'val(to_integer(unsigned(to_stdlogicvector(vector_i((byte_v+1)*8-1 downto (byte_v+0)*8)))));
-          write(dump_f, data_v);
-        end loop;
+        write_bin(dump_f, array_i(index_v));
       end loop;
     end if;
     file_close(dump_f);
@@ -75,31 +90,26 @@ package body neorv32_hdldb_io_pkg is
 
   -- dump array_std_logic_vector to plain binary file --
   procedure dump_bin(
-    file_name : string;
-    vector_i  : std_logic_vector;
-    mode      : file_open_kind := WRITE_MODE
+    file_name : in  string;
+    vector_i  : in  std_logic_vector;
+    mode      : in  file_open_kind := WRITE_MODE
   ) is
-    type char_file is file of character;
     file     dump_f : char_file;
     variable data_v : character;
   begin
     if (file_name /= "") then
       file_open(dump_f, file_name, mode);
-      for byte_v in 0 to vector_i'length/8-1 loop
-        data_v := character'val(to_integer(unsigned(vector_i((byte_v+1)*8-1 downto (byte_v+0)*8))));
-        write(dump_f, data_v);
-      end loop;
+      write_bin(dump_f, vector_i);
     end if;
     file_close(dump_f);
   end procedure dump_bin;
 
   -- dump array_std_logic_vector array to plain binary file --
   procedure dump_bin(
-    file_name : string;
-    array_i   : array_std_logic_vector;
-    mode      : file_open_kind := WRITE_MODE
+    file_name : in  string;
+    array_i   : in  array_std_logic_vector;
+    mode      : in  file_open_kind := WRITE_MODE
   ) is
-    type char_file is file of character;
     file     dump_f   : char_file;
     variable vector_i : array_i'element'subtype;
     variable data_v   : character;
@@ -107,11 +117,7 @@ package body neorv32_hdldb_io_pkg is
     if (file_name /= "") then
       file_open(dump_f, file_name, mode);
       for index_v in array_i'range loop
-        vector_i := array_i(index_v);
-        for byte_v in 0 to vector_i'length/8-1 loop
-          data_v := character'val(to_integer(unsigned(vector_i((byte_v+1)*8-1 downto (byte_v+0)*8))));
-          write(dump_f, data_v);
-        end loop;
+        write_bin(dump_f, array_i(index_v));
       end loop;
     end if;
     file_close(dump_f);
